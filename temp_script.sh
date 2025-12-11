268fde9705f3014fc78670cd7a66a0a5beb66769
Manuel Campos <mcampos.cerda@gmail.com>
2025-12-11 14:55:24 -0600

    🚫 Ignore unrelated HTML file and remove temp script

--- temp_script.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cd /Users/mcampos.cerda/Documents/Programming/ai-team-router && git add . && git commit -m "SECURITY: Emergency cleanup - API keys removed" && git push --force origin main
